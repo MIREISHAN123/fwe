fe9f29b7305312f4b49a40e1337e92c7e53cc7c2
MIREISHAN123 <muhammadareebalmutahar3@gmail.com>
2025-12-18 20:28:58 +0530

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,21 @@
+class cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"Iam a cat. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("Meow")
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+    def make_sound(self):
+        print("bark")
+cat1 = cat("Dodo", 2.5)
+dog1 = Dog("Tyson", 8)
+for animal in (cat1, dog1):
+    animal.make_sound()
+    animal.info()            
